101cd232203ff140d1df5dd602331283d8137002
IzsuddinAtar <IzsuddinAtar>
2023-04-13 14:23:45 +0700

    Add classes and activity to the project

---
+++ src/main/java/com/example/visualnovelproject/page10.kt
@@ -0,0 +1,11 @@
+package com.example.visualnovelproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class page10 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page10)
+    }
+}
